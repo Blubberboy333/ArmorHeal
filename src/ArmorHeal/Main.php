1affbdb2914c762ea5e9a95ec7791c829b81d0a1
Blubberboy333 <thecowmooman@gmail.com>
2015-05-20 18:03:03 -0400

    Update Main.php

--- src/ArmorHeal/Main.php
+++ src/ArmorHeal/Main.php
@@ -1,1 +1,37 @@
+<?php
 
+namespace ArmorHeal\Main;
+
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityArmorChangeEvent;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+  public function onEnable{
+    $this->saveDefaultConfig();
+    $this->getServer->info(TextFormat::BLUE . "ArmorHeal enabled");
+  }
+  public function onDisable(){
+    $this->getServer->info(TextFormat::RED . "ArmorHeal disabled");
+  }
+  
+  public function onEntityArmorChangeEvent(EntityArmorChangeEvent $event){
+    $entity = $event->getEntity();
+    if($entity instanceof Player){
+      if($entity->hasPermission("armorheal.heal")){
+        $armor = $event->getNewItem();
+        $heal = $this->getConfig()->get("Armor");
+        if($armor = $heal){
+          $entity->setHealth(20);
+        }else{
+        }
+      }else{
+      }
+    }else{
+    }
+  }
+}
